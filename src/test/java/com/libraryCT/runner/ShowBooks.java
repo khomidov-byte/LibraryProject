f6a6c5d5f016aa2cbf74dc70723c5b346f520937
Mousa Adolai <mousa.adolai@gmail.com>
2021-11-12 08:54:16 -0500

    completed user story 4

--- src/test/java/com/libraryCT/runner/ShowBooks.java
+++ src/test/java/com/libraryCT/runner/ShowBooks.java
@@ -6,11 +6,8 @@ public class ShowBooks {
 
     public static void main(String[] args) {
         DB_Util.createConnection();
-        DB_Util.runQuery("SELECT bc.name, COUNT(*)  FROM books b" +
-                " INNER JOIN book_categories bc on b.book_category_id = bc.id" +
-                " INNER JOIN book_borrow bb on bb.book_id = b.id"+
-                " GROUP BY bc.name"+
-                " ORDER BY COUNT(*) DESC");
+        DB_Util.runQuery("SELECT DISTINCT u.id, u.full_name FROM users u" +
+                " INNER JOIN book_borrow bb on u.id = bb.user_id" );
         DB_Util.displayAllData();
     }
 }
